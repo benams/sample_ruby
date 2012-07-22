7b517356f9314b22435ba4f92c284a660a1cca85
Ben Amsalem <benams@gmail.com>
2012-07-22 13:43:18 +0300

    Finnish layout and routes

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+
+  describe "full_title" do
+    it "should include the page title" do
+      full_title("ben").should =~ /ben/
+    end 
+
+    it "should include the base title" do
+      full_title("ben").should =~ /Sample App/
+    end 
+  
+    it "should not include a bar for thr home page" do
+      full_title("").should =~ /\|/
+    end 
+  end
+end
