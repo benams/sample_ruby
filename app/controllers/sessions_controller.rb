998d92ae92153b5442c7369bab1fec390deb6439
Ben Amsalem <benams@gmail.com>
2012-07-24 20:45:00 +0300

    Finnish user actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       sign_in user 
-      redirect_to user
+      redirect_back_or user
     else
-      # add an error to the login page
       flash.now[:error] = 'Invalid email/password combination' 
       render 'new'
     end
